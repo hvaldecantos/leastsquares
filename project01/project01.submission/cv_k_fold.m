4c17b794959e091684b02e8c1c675f6c3748fb78
hector <hvaldecantos@gmail.com>
2018-09-24 11:35:37 -0400

    creates a function to get the folds

--- project01/project01.submission/cv_k_fold.m
+++ project01/project01.submission/cv_k_fold.m
@@ -21,12 +21,8 @@ for p=0:max_p
     train_error_acc = 0;
     test_error_acc = 0;
     
-    for k=1:K
-        i_start = sum(fold_sizes(1:k-1)) + 1;
-        i_end = i_start + fold_sizes(k) - 1;
-
-        X_tr = X([1:i_start-1 i_end+1:N], :);   y_tr = y([1:i_start-1 i_end+1:N], :);
-        X_te = X(i_start:i_end, :);  y_te = y(i_start:i_end, :);
+    for k=1:K       
+        [X_tr, y_tr, X_te, y_te] = get_folds(X, y, fold_sizes, k);
         
         Z_tr = expand(poly, X_tr);
         [M, R_tr, w] = least_squares(Z_tr, y_tr);
